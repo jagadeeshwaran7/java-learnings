c9235c10a18b745aa9c72249efa84e59891c9bb0
jagadeesh waran <87805745+jagadeeshwaran7@users.noreply.github.com>
2023-07-22 04:40:13 +0530

    Update MissingNumbers.java
    Renamed method

--- array/MissingNumbers.java
+++ array/MissingNumbers.java
@@ -5,7 +5,7 @@ import com.sun.istack.internal.NotNull;
 public class MissingNumbers {
     // To find a missing number in an array
 //Time Complexity=O(n)
-    public static int toFindOneMissingNumber(@NotNull int[] array, @NotNull int length) {
+    public static int toFindAMissingNumber(@NotNull int[] array, @NotNull int length) {
         int sumOfArray = 0;
         int expectedSum = length * ((length + 1) / 2);
         for (int i : array) {
